4df9e8523ddf5446e0591b145eca3b527fccd427
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-10-26 14:32:24 +0100

    Prevent .eslintcache hot reload loop

--- frontend/frontend/nuxt.config.js
+++ frontend/frontend/nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    ['@nuxtjs/eslint-module', { cache: false }],
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
