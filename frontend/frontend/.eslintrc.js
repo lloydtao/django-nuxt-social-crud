19b42c0b7fbb36a86e11b5f50d00a8d1954072a0
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-10-26 14:09:49 +0100

    Initialise Nuxt project

---
+++ frontend/frontend/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    'jest/globals': true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: ['jest'],
+  // add your custom rules here
+  rules: {},
+}
